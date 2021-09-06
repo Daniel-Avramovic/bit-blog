62d8651751f84484692cb14e1faf7996bb8d5ea5
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-07 01:45:05 +0200

    finished authors componet

--- src/app/authors/Authors.jsx
+++ src/app/authors/Authors.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
-
+import React, { Fragment, useEffect, useState } from "react";
+import { getAuthors } from "../../services/getAuthors";
+import AuthorsUi from '../UI/authors'
+import Loader from "../loader/Loader";
 const Authors = () => {
-    return(
-        <h1>Authors</h1>
-    )
-}
+  const [authors, setAuthors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const allAuthors = async () => {
+    const newAuthors = await getAuthors();
+    console.log(newAuthors);
+    setAuthors(newAuthors);
+    setLoading(false);
+  };
+
+  useEffect(() => {allAuthors()},[])
+
+  return (
+      <Fragment>
+          {loading && <Loader />}
+          <AuthorsUi authors={authors} />
+      </Fragment>
+  )
+};
 
 export default Authors;
