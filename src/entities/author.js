48ce104d11d514ad76edaeb43d77d9e9bec21ea2
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-07 17:33:04 +0200

    create classes for geo-location, comapny, and address

--- src/entities/author.js
+++ src/entities/author.js
@@ -9,7 +9,35 @@ export class Author {
         this.website = website;
         this.company = company;
     }
+};
+export class Address {
+    constructor(street, city, zipcode, geo){
+        this.street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.geo = geo;
+    }
 }
 
+export class Geo{
+    constructor(lat, lng){
+    this.lat = lat;
+    this.lng = lng;
+    }
+}
+export class Company{
+    constructor(name, slogan){
+this.name = name;
+this.slogan = slogan;
+    }
+}
 
-
+// "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
